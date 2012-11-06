818f243f8cb327eb25208e36695224c50d76a845
glebk <gleb.kuznetsov@gmail.com>
2012-11-05 22:24:05 -0500

    Add markdown.css for default clean style.

--- markdown_watchdog.py
+++ markdown_watchdog.py
@@ -6,12 +6,14 @@ in response to each change (save) event.
 """
 
 import os
-from subprocess import call
+import subprocess
 import sys
 import time
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+
+MARKDOWN_CSS_LINK = '<link href="http://kevinburke.bitbucket.org/markdowncss/markdown.css" rel="stylesheet"></link>'
 
 class MarkdownCompilerEventHandler(FileSystemEventHandler):
   """EventHandler that runs markdown whenever any .md file changes."""
@@ -20,7 +22,10 @@ class MarkdownCompilerEventHandler(FileSystemEventHandler):
     (src_root, src_ext) = os.path.splitext(src)
     if src_ext == '.md':
       outfile = src_root + '.html'
-      call(['markdown_py', event.src_path, '-f', outfile])
+      outfile_fh = open(outfile, 'w')
+      outfile_fh.write(MARKDOWN_CSS_LINK + '\n\n')
+      subprocess.call(['markdown_py', event.src_path], stdout=outfile_fh)
+      outfile_fh.close()
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
